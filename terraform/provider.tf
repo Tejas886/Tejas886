9b7d41d4e1a2c49fe8bc6d62a4dd95e90a3ca1a9
kmov <tejabkbb@gmail.com>
2026-03-06 23:50:36 +0530

    committed

---
+++ terraform/provider.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  region = "us-east-1"
+  name   = "tejas-eks-cluster"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
